905d18231ae1e71385e6cad881aaac2aaebf3400
RickII R <f13.rick@gmail.com>
2022-09-20 18:19:51 -0300

    Stock Management is UPDATED 0002

--- src/stock/StockManagementService.java
+++ src/stock/StockManagementService.java
@@ -2,5 +2,6 @@ public class StockManagementService{
     
     public static void main(String[] args) {
         System.out.println("It's the Stock Management Service!");
+        System.out.println("The Stock Management Service is working!");
     }
 }
